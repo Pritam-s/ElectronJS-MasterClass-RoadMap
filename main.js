f52441309ba40e0820f9ac9953e8412993ca6a94
Pritam Singh <singhpritam1704@gmail.com>
2022-07-19 15:17:09 +0530

    10. Frameless app and some important CSS properties.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ function createWindow () {
   const win = new BrowserWindow({
     width: 800, // width of the window
     height: 600, // height of the window
+    frame: false, // remove the frame
     
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
@@ -17,11 +18,3 @@ function createWindow () {
 
 app.on('ready', createWindow) // when the app is ready, create the window
 
-app.on('browser-window-created', () => {console.warn("browser-window-created")})
-//when a browser window is created, log a message to the console
-
-app.on('browser-window-focus', () => {console.warn("browser-window-focus")})
-//when a browser window is focused, log a message to the console
-
-app.on('before-quit', () => {console.warn("App is about to exit")})
-//when the app is about to quit, log a message to the console
