bd230f17bd00d83b4420aa0f9adf299d73bbd400
Pritam Singh <singhpritam1704@gmail.com>
2022-07-19 16:06:53 +0530

    11. Window State (Installing npm package)

--- main.js
+++ main.js
@@ -1,19 +1,29 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
+const windowStateKeeper = require('electron-window-state');
+let win;
 
-function createWindow () {
-  const win = new BrowserWindow({
-    width: 800, // width of the window
-    height: 600, // height of the window
-    frame: false, // remove the frame
+function createWindow() {
+  let mainWindowState = windowStateKeeper({
+    defaultWidth: 800,
+    defaultHeight: 600
+  });
+
+
+  win = new BrowserWindow({
+    'x': mainWindowState.x,
+    'y': mainWindowState.y,
+    'width': mainWindowState.width,
+    'height': mainWindowState.height,
     
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
-  })
-  
+  });
+
   win.loadFile('index.html')
-//   win.webContents.openDevTools() // open dev tools
+  //   win.webContents.openDevTools() // open dev tools
+  mainWindowState.manage(win); // manage window state
 }
 
 app.on('ready', createWindow) // when the app is ready, create the window
