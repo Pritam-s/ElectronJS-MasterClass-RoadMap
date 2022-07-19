f20eccae5b9bba77416dc058ffada933e9267bc8
Pritam Singh <singhpritam1704@gmail.com>
2022-07-19 13:29:45 +0530

    7. Child Window

--- main.js
+++ main.js
@@ -3,17 +3,17 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 200, // width of the window
-    height: 500, // height of the window
-    backgroundColor: "#C4C4C4", // background color of the window
-    alwaysOnTop: true, // make the window always on top
-    title:"Electron MasterClass", // title of the window
-    resizable: false, // disable resizing of the window
-    frame: false, // remove the window frame
+    width: 800, // width of the window
+    height: 600, // height of the window
+    
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
+// child process and child window
+  let child = new BrowserWindow({ parent: win})
+  child.loadFile('child.html')
+//   child.show()
 
   win.loadFile('index.html')
 //   win.webContents.openDevTools() // open dev tools
