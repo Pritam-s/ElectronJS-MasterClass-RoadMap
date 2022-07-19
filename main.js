18d6feb3c32536b6e4078b38444cce07da6594eb
Pritam Singh <singhpritam1704@gmail.com>
2022-07-19 13:10:05 +0530

    6. Browser Window Properties.

--- main.js
+++ main.js
@@ -3,15 +3,20 @@ const path = require('path')
 
 function createWindow () {
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 200, // width of the window
+    height: 500, // height of the window
+    backgroundColor: "#C4C4C4", // background color of the window
+    alwaysOnTop: true, // make the window always on top
+    title:"Electron MasterClass", // title of the window
+    resizable: false, // disable resizing of the window
+    frame: false, // remove the window frame
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
     }
   })
 
   win.loadFile('index.html')
-  win.webContents.openDevTools() // open dev tools
+//   win.webContents.openDevTools() // open dev tools
 }
 
 app.whenReady().then(createWindow)
